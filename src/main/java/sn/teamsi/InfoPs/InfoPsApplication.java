07cde452147e57e84d29ce8f5a3bb839b4aaab24
Sidy SY <sidysy2013@gmail.com>
2019-08-31 18:19:24 +0200

    initial commit

---
+++ src/main/java/sn/teamsi/InfoPs/InfoPsApplication.java
@@ -0,0 +1,13 @@
+package sn.teamsi.InfoPs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoPsApplication.class, args);
+	}
+
+}
